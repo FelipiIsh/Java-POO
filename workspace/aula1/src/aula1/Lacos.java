de3fd96283787c34a48f7d70fd113df55955167e
FelipiIsh <129989699+FelipiIsh@users.noreply.github.com>
2023-06-07 18:32:45 -0300

    Add files via upload

---
+++ workspace/aula1/src/aula1/Lacos.java
@@ -0,0 +1,19 @@
+package aula1;
+
+public class Lacos {
+
+	public static void main(String[] args) {
+
+		int i = 0;
+		int soma = 0;
+				
+		while (i <= 100) {
+			soma = soma + i;
+			i++;
+					
+		}
+		
+		System.out.println("Soma: " + soma);
+	}
+
+}
