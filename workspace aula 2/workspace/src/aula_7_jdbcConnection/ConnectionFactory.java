20d475ebd68506733f13f4dddd1d0b41304f064d
FelipiIsh <129989699+FelipiIsh@users.noreply.github.com>
2023-06-27 18:31:02 -0300

    Add files via upload

--- workspace aula 2/workspace/src/aula_7_jdbcConnection/ConnectionFactory.java
+++ workspace aula 2/workspace/src/aula_7_jdbcConnection/ConnectionFactory.java
@@ -8,7 +8,7 @@ public class ConnectionFactory {
 
 	public Connection conectar() {
 		try {
-			return DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", "TM_Felipi_Ishida","021004");
+			return DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", "usuario","senha");
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
